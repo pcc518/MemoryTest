da8bf8251af0953d0c6967580a0371e1f211ae07
pengchenchen <1509230568@qq.com>
2018-07-20 10:39:17 +0800

    初始化

---
+++ MemoryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryTest
+//
+//  Created by huijin on 2018/7/20.
+//  Copyright © 2018年 huijin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryTest
+//
+//  Created by huijin on 2018/7/20.
+//  Copyright © 2018年 huijin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
